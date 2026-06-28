e7846bf71098e47965f9d45bc0b9dfefe33b04e6
Cosmow22 <cosmow543@gmail.com>
2026-06-28 19:11:21 +0200

    add elapsed time

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import requests
 import json
+from time import time
 
 
 your_input = input("Votre nombre :")
-r = requests.get(f"http://localhost:3000/{your_input}")
 
+t1 = time()
+r = requests.get(f"http://localhost:3000/{your_input}")
+t2 = time()
 is_prime = json.loads(r.content.decode('utf-8'))["response"]
 
 if is_prime :
     print(f"{your_input} is prime number.")
 else :
     print(f"{your_input} is NOT prime number.")
+print(f"Temps écoulé : {t2-t1}")
